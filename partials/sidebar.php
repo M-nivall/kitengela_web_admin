e0bed2fb74a079f1831767738ad07e9b66ee26d8
Nivall <nivahmichael@gmail.com>
2025-11-23 15:40:31 +0300

    updated the sidabar details

--- partials/sidebar.php
+++ partials/sidebar.php
@@ -111,6 +111,7 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
 <!-- Rotate Arrows Script -->
+ 
 <script>
   document.querySelectorAll('[data-bs-toggle="collapse"]').forEach(trigger => {
     const icon = trigger.querySelector('.bi-chevron-right');
